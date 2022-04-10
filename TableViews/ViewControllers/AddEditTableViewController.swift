8123b7ec49c648d0ac39be2124d94db1f03434a4
Gerodot <gerodot@me.com>
2022-04-11 00:17:13 +0300

    Struct Project to MVC && Create UI in Story Board && Create ViewControllers

---
+++ TableViews/ViewControllers/AddEditTableViewController.swift
@@ -0,0 +1,12 @@
+//
+//  AddEditTableViewController.swift
+//  TableViews
+//
+//  Created by Gerodot on 4/11/22.
+//
+
+import UIKit
+
+class AddEditTableViewController: UITableViewController {
+
+}
---
+++ TableViews/ViewControllers/EmojiTableViewController.swift
@@ -0,0 +1,12 @@
+//
+//  EmojiTableViewController.swift
+//  TableViews
+//
+//  Created by Gerodot on 4/11/22.
+//
+
+import UIKit
+
+class EmojiTableViewController: UITableViewController {
+
+}
