73702ab7c1f29240d073b2531ebdbf223ab69f8c
Gerodot <gerodot@me.com>
2022-04-16 18:43:37 +0300

    Changed the encoding from PLIST to JSON for data storage. Renamed the project to Emoji Dictionary

--- TableViews/Managers/DataManager.swift
+++ TableViews/Managers/DataManager.swift
@@ -11,21 +11,21 @@ class DataManager {
     
     var archiveURL: URL? {
         guard let documentDictonary = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil }
-        return documentDictonary.appendingPathComponent("emojis").appendingPathExtension("plist")
+        return documentDictonary.appendingPathComponent("emojis").appendingPathExtension("json")
         
     }
     func loadEmojis () -> [Emoji]?{
         guard let archiveURL = archiveURL else {return nil}
         guard let encodedEmoji = try? Data(contentsOf: archiveURL) else { return nil }
         
-        let decoder = PropertyListDecoder()
+        let decoder = JSONDecoder()
         return try? decoder.decode([Emoji].self, from: encodedEmoji)
     }
     
     func saveEmojis(_ emojis: [Emoji]){
         guard let archiveURL = archiveURL else {return}
         
-        let encoder = PropertyListEncoder()
+        let encoder = JSONEncoder()
         guard let encodedEmoji = try? encoder.encode(emojis) else {return}
         
         try? encodedEmoji.write(to: archiveURL, options: .noFileProtection)
