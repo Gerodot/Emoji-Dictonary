49c9bd488fa83dedaed016c80adeb189cc3e6ddc
Gerodot <gerodot@me.com>
2022-04-17 23:43:00 +0300

    Add validation

--- TableViews/ViewControllers/AddEditTableViewController.swift
+++ TableViews/ViewControllers/AddEditTableViewController.swift
@@ -14,8 +14,9 @@ class AddEditTableViewController: UITableViewController {
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var descriptionTextField: UITextField!
     @IBOutlet weak var usageTextField: UITextView!
-    @IBOutlet weak var titleLabel: UINavigationItem!
     
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    @IBOutlet weak var titleLabel: UINavigationItem!
     
     //MARK: - Properites
     var emoji = Emoji()
@@ -23,24 +24,18 @@ class AddEditTableViewController: UITableViewController {
     //MARK: - UIViewController
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupTextFields()
         updateUI()
     }
     
     //MARK: - Methods
-    func updateUI() {
-        symbolTextField.text = emoji.symbol
-        nameTextField.text = emoji.name
-        descriptionTextField.text = emoji.description
-        usageTextField.text = emoji.usage
-        titleName()
-        
-    }
     
     func titleName() {
         if emoji.symbol != "" && emoji.name != "" && emoji.description != "" && emoji.usage != "" {
             titleLabel.title = "Edit"
         }
     }
+    
     
     func saveEmoji() {
         emoji.symbol = symbolTextField.text ?? ""
@@ -49,8 +44,43 @@ class AddEditTableViewController: UITableViewController {
         emoji.usage = usageTextField.text ?? ""
     }
     
+    func updateUI() {
+        symbolTextField.text = emoji.symbol
+        nameTextField.text = emoji.name
+        descriptionTextField.text = emoji.description
+        usageTextField.text = emoji.usage
+        titleName()
+    }
+    
+    func setupTextFields() {
+        symbolTextField.addTarget(self, action: #selector(self.textFieldDidChange(_:)), for: UIControl.Event.editingChanged)
+        nameTextField.addTarget(self, action: #selector(self.textFieldDidChange(_:)), for: UIControl.Event.editingChanged)
+        descriptionTextField.addTarget(self, action: #selector(self.textFieldDidChange(_:)), for: UIControl.Event.editingChanged)
+        //usageTextField.textFieldDidChange(self, action: #selector(self.textFieldDidChange(_:)), for: UIControl.Event.editingChanged)
+    }
+    
     //MARK: - Navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         saveEmoji()
+    }
+    
+    @objc func textFieldDidChange(_ textField: UITextField) {
+        guard !symbolTextField.text!.isEmpty else {
+            saveButton.isEnabled = false
+            return
+        }
+        guard !nameTextField.text!.isEmpty else {
+            saveButton.isEnabled = false
+            return
+        }
+        guard !descriptionTextField.text!.isEmpty else {
+            saveButton.isEnabled = false
+            return
+        }
+        guard !usageTextField.text!.isEmpty else {
+            saveButton.isEnabled = false
+            return
+        }
+        saveButton.isEnabled = true
     }
 }
